40efde17893ea8b38a62ba7fd6a46c52f93d3dc4
Carol Liu <carol@carols-mac-mini.lan>
2022-06-30 21:27:31 -0700

    Initial files

---
+++ Itinerary/Shared/ItineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItineraryApp.swift
+//  Shared
+//
+//  Created by Carol Liu on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ItineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
